62b8b488b5a3d3cb144b7ab44d01420983a80e03
Aakash Shukla <shukla.aa@northeastern.edu>
2023-01-22 16:14:43 -0500

    Added fault handling services

--- RequestService/Controllers/RequestController.cs
+++ RequestService/Controllers/RequestController.cs
@@ -1,23 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
-
+using RequestService.Policies;
 namespace RequestService.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class RequestController : ControllerBase
     {
-        // private readonly ClientPolicy _clientPolicy;
-        // private readonly IHttpClientFactory _clientFactory;
-        // public RequestController(ClientPolicy clientPolicy, IHttpClientFactory clientFactory)
-        // {
-        //     _clientPolicy = clientPolicy;
-        //     _clientFactory = clientFactory;
-        // }
+        private readonly ClientPolicy _clientPolicy;
+        private readonly IHttpClientFactory _clientFactory;
+        public RequestController(ClientPolicy clientPolicy, IHttpClientFactory clientFactory)
+        {
+            _clientPolicy = clientPolicy;
+            _clientFactory = clientFactory;
+        }
+
         [HttpGet]
         public async Task<ActionResult> MakeRequest()
         {
-            var client = new HttpClient();
-            var response = await client.GetAsync("https://localhost:7060/api/v1/response/70");
+            var client = _clientFactory.CreateClient();
+            var response = await _clientPolicy.ImmediateHttpRetry.ExecuteAsync(() => client.GetAsync("https://localhost:7060/api/v1/response/10"));
 
             if(response.IsSuccessStatusCode)
             {
--- RequestService/Program.cs
+++ RequestService/Program.cs
@@ -1,11 +1,13 @@
+using RequestService.Policies;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddHttpClient();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<ClientPolicy>(new ClientPolicy());
 
 var app = builder.Build();
 
